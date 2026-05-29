edeb293395fb642ece0219f47197044391981dde
Romanov Vyacheslav <rvr280206@gmail.com>
2026-05-29 21:00:04 +0300

    feat(integers): min element after replacement solved

---
+++ src/main/kotlin/com/github/slavikjunior/integers/MinimumElementAfterReplacementWithDigitSum.kt
@@ -0,0 +1,32 @@
+package com.github.slavikjunior.integers
+
+class Solution {
+
+    companion object {
+
+        fun minElement(nums: IntArray): Int {
+            return replaceElements(nums).min()
+        }
+
+        fun replaceElements(nums: IntArray): IntArray {
+            return nums.map {
+               it.toSumOfDigits()
+            }.toIntArray()
+        }
+    }
+}
+
+fun main() {
+
+    val test = intArrayOf(1123, 23, 34, 45)
+    println(Solution.minElement(test))
+}
+
+fun Int.toSumOfDigits(): Int {
+    var sum = 0
+    this.toString().toCharArray().forEach {
+        sum += it.digitToInt()
+    }
+
+    return sum
+}
