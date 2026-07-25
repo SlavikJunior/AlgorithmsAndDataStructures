cedfde24694f359db76e68376cfeeafe755444d5
Arthas <rvr280206@gmail.com>
2026-07-25 09:16:22 +0400

    SquaresOfSortedArray optimized solution

--- src/main/kotlin/com/github/slavikjunior/arrays/SquaresOfSortedArray.kt
+++ src/main/kotlin/com/github/slavikjunior/arrays/SquaresOfSortedArray.kt
@@ -1,16 +1,40 @@
 package com.github.slavikjunior.arrays
 
+import kotlin.math.absoluteValue
+
 class SquaresOfSortedArraySolution {
 
     companion object {
 
         fun sortedSquares(nums: IntArray): IntArray {
-            return nums.map { it * it }.sorted().toIntArray()
+            val result = IntArray(nums.size)
+            var resultInsertionIndex = result.lastIndex
+
+            var leftIndex = 0
+            var rightIndex = nums.lastIndex
+
+            while(leftIndex <= rightIndex) {
+                val leftElem = nums[leftIndex]
+                val rightElem = nums[rightIndex]
+
+                if (leftElem.absoluteValue > rightElem.absoluteValue) {
+                    result[resultInsertionIndex] = leftElem * leftElem
+                    leftIndex++
+                }
+                else {
+                    result[resultInsertionIndex] = rightElem * rightElem
+                    rightIndex--
+                }
+
+                resultInsertionIndex--
+            }
+
+            return result
         }
     }
 }
 
 fun main() {
 
-    println(SquaresOfSortedArraySolution.sortedSquares(intArrayOf(-4, -1, 0, 3, 10)).toList())
+    println(SquaresOfSortedArraySolution.sortedSquares(intArrayOf(-7,-3,2,3,11)).toList())
 }
