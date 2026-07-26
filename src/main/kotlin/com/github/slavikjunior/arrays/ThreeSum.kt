5b96290f6cb73d64915a3e85c176ebea522253ad
Arthas <rvr280206@gmail.com>
2026-07-26 07:59:12 +0400

    three sum solved

---
+++ src/main/kotlin/com/github/slavikjunior/arrays/ThreeSum.kt
@@ -0,0 +1,47 @@
+package com.github.slavikjunior.arrays
+
+
+class ThreeSumSolution {
+    companion object {
+        fun threeSum(nums: IntArray): List<List<Int>> {
+            val result = mutableListOf<List<Int>>()
+
+            nums.sort()
+
+            for (i in nums.indices) {
+
+                if (nums[i] > 0) break
+
+                if (i > 0 && nums[i] == nums[i - 1]) continue
+
+                var left = i + 1
+                var right = nums.lastIndex
+
+                while (left < right) {
+                    val sum = nums[i] + nums[left] + nums[right]
+
+                    if (sum == 0) {
+                        result.add(listOf(nums[i], nums[left], nums[right]))
+
+                        while (left < right && nums[left] == nums[left + 1]) left++
+                        while (left < right && nums[right] == nums[right - 1]) right--
+
+                        left++
+                        right--
+                    } else if (sum < 0) {
+                        left++
+                    } else {
+                        right--
+                    }
+                }
+            }
+
+            return result
+        }
+    }
+}
+
+fun main() {
+
+    println(ThreeSumSolution.threeSum(intArrayOf(-1, 0, 1, 2, -1, -4)))
+}
