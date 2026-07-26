414ba2a30758be26d5e7888e0463347119b56697
Arthas <rvr280206@gmail.com>
2026-07-26 19:34:12 +0400

    maximum product of three numbers optimized  solution

--- src/main/kotlin/com/github/slavikjunior/arrays/MaximumProductOfThreeNumbers.kt
+++ src/main/kotlin/com/github/slavikjunior/arrays/MaximumProductOfThreeNumbers.kt
@@ -7,10 +7,35 @@ class MaximumProductOfThreeNumbersSolution {
     companion object {
 
         fun maximumProduct(nums: IntArray): Int {
-            nums.sort()
+            var min1 = Int.MAX_VALUE
+            var min2 = Int.MAX_VALUE
 
-            val firstProduct = nums[nums.size - 1] * nums[nums.size - 2] * nums[nums.size - 3]
-            val secondProduct = nums[nums.size - 1] * nums[0] * nums[1]
+            var max1 = Int.MIN_VALUE
+            var max2 = Int.MIN_VALUE
+            var max3 = Int.MIN_VALUE
+
+            for (current in nums) {
+                if (current > max1) {
+                    max3 = max2
+                    max2 = max1
+                    max1 = current
+                } else if (current > max2) {
+                    max3 = max2
+                    max2 = current
+                } else if (current > max3) {
+                    max3 = current
+                }
+
+                if (current < min1) {
+                    min2 = min1
+                    min1 = current
+                } else if (current < min2) {
+                    min2 = current
+                }
+            }
+
+            val firstProduct = min1 * min2 * max1
+            val secondProduct = max1 * max2 * max3
 
             return max(firstProduct, secondProduct)
         }
