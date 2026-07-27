701a2a592ad846bb0bec3d4e58bc723f93d08ce3
Romanov Vyacheslav <rvr280206@gmail.com>
2026-07-27 10:25:50 +0400

    MaximumProductOfTwoElementsInArraySolution optimized solution

--- src/main/kotlin/com/github/slavikjunior/arrays/MaximumProductOfTwoElementsInArray.kt
+++ src/main/kotlin/com/github/slavikjunior/arrays/MaximumProductOfTwoElementsInArray.kt
@@ -1,15 +1,30 @@
 package com.github.slavikjunior.arrays
 
+import kotlin.math.max
+
 class MaximumProductOfTwoElementsInArraySolution {
 
     companion object {
 
         fun maxProduct(nums: IntArray): Int {
-            nums.sort()
-            return (nums.last() - 1) * (nums.preLast() - 1)
-        }
+            var max1 = 0
+            var max2 = 0
 
-        private fun IntArray.preLast(): Int = this[this.lastIndex - 1]
+            var maxProduct = 0
+
+            nums.forEach { current ->
+                if (current > max1) {
+                    max2 = max1
+                    max1 = current
+                } else if (current > max2) {
+                    max2 = current
+                }
+
+                maxProduct = max(maxProduct, (max1 - 1) * (max2 - 1))
+            }
+
+            return maxProduct
+        }
     }
 }
 
