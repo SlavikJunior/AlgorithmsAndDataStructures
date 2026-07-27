24f57464d5e647ea3d06123fc04e0a2a85832333
Romanov Vyacheslav <rvr280206@gmail.com>
2026-07-27 09:56:30 +0400

    maximum product of three numbers simple solution writen on java

---
+++ src/main/kotlin/com/github/slavikjunior/arrays/MaximumProductOfThreeNumbersJavaSolution.java
@@ -0,0 +1,25 @@
+package com.github.slavikjunior.arrays;
+
+import java.util.Arrays;
+
+public class MaximumProductOfThreeNumbersJavaSolution {
+
+    public static int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int min1 = nums[0];
+        int min2 = nums[1];
+
+        int max1 = nums[nums.length - 1];
+        int max2 = nums[nums.length - 2];
+        int max3 = nums[nums.length - 3];
+
+        int firstProduct = max1 * max2 * max3;
+        int secondProduct = max1 * min1 * min2;
+
+        return Math.max(firstProduct, secondProduct);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(MaximumProductOfThreeNumbersJavaSolution.maximumProduct(new int[]{1, 2, 3, 4}));
+    }
+}
